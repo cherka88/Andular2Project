05c6a8ee5f1babd7ed171f0d6f2a7543b571be60
lin profiteer <terence@linde-MacBook-Pro.local>
2017-02-11 23:02:46 +0800

    任務 15：使用 Http 服務元件呼叫 RESTful APIs

--- demo20170210/src/app/app.component.ts
+++ demo20170210/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Headers, RequestOptions,  Http} from '@angular/http';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,22 @@ export class AppComponent {
   inputHint = 'What needs to be done?';
   todos:any[] = [];
   todo:string;
+  constructor(private http: Http){
+
+  }
+  ngOnInit(){
+    let headers = new Headers({
+      'Authorization': 'token 4b781c74-793e-4bae-b543-cecf3195a119'
+    });
+    let options = new RequestOptions({
+      headers: headers
+    })
+
+    this.http.get('/me/todo20170210',options)
+     .subscribe(res=>{
+       this.todos = res.json();
+     });
+  }
   add(item:HTMLInputElement){
     // this.todos.push({
     //   value: item.value,
@@ -19,6 +36,29 @@ export class AppComponent {
   }
   clearCompleted(){
     this.todos = this.todos.filter(n=>!n.isDone);
+  }
+  remove(item){
+    let idx = this.todos.indexOf(item);
+    this.todos.splice(idx,1);
+    this.todos = [...this.todos];
+  }
+  toggleAll(status:boolean){
+    this.todos = this.todos.map(s=>{
+      s.isDone = status;
+      return s;
+    });
+  }
+  save(){
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': 'token 4b781c74-793e-4bae-b543-cecf3195a119'
+    });
+    let options = new RequestOptions({
+      headers: headers
+    })
+
+    this.http.post('/me/todo20170210', this.todos, options)
+     .subscribe();
   }
 
 }
