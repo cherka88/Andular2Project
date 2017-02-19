4c1262f0f4d035828eb479e42f5cd4eee7c29070
lin profiteer <terence@pc227.wireless4.tku.edu.tw>
2017-02-19 12:59:02 +0800

    20 練習透過 FormArray 搭配 *ngFor 產生動態表單結構

--- ng2-advanced/src/app/reactiveform/reactiveform.component.ts
+++ ng2-advanced/src/app/reactiveform/reactiveform.component.ts
@@ -1,4 +1,4 @@
-import {Validators, FormGroup,  FormBuilder} from '@angular/forms';
+import {FormArray, Validators,  FormGroup,   FormBuilder} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,7 +10,13 @@ export class ReactiveformComponent implements OnInit {
   form: FormGroup;
   constructor(private fb:FormBuilder) {
     this.form = this.fb.group({
-      'name': ['123',Validators.required]
+      'name': ['123',[Validators.required,Validators.minLength(3)]],
+      'group1': this.fb.array([
+        this.fb.control('haha1',Validators.required),
+        this.fb.control('haha2',Validators.required),
+        this.fb.control('haha3',Validators.required),
+        this.fb.control('haha4',Validators.required),
+      ])
     });
   }
 
@@ -20,6 +26,8 @@ export class ReactiveformComponent implements OnInit {
 
   ngOnInit() {
     this.form.addControl('email', this.fb.control('default@example.com', Validators.required));
+    let group1: FormArray = <FormArray>this.form.controls['group1'];
+    group1.insert(group1.length,this.fb.control('haha5',Validators.required));
   }
 
 }
